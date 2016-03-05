eee075ae63fe263a5b90e80b6a91bb7957e7d1f8
fttang <mictang@uw.edu>
2016-03-04 21:14:34 -0800

    Read income grouped COD data, function

--- ReadIncomeCOD.m
+++ ReadIncomeCOD.m
@@ -1,19 +1,14 @@
-function [IncomeCOD] = IncomeCODProcess(IncomeCOD)
+function [IncomeCOD] = ReadIncomeCOD(IncomeCOD)
 
 IncomeCOD = sortrows(IncomeCOD,'Causes','ascend');
 IncomeCOD = sortrows(IncomeCOD, 'Numeric', 'ascend');
 IncomeCOD = sortrows(IncomeCOD, 'Year', 'ascend');
-
 
 % this could be written as a function
 IncomeCOD = table2array(IncomeCOD);
 IncomeCOD = IncomeCOD(strcmpi(IncomeCOD(:, 5), 'Both sexes'), :);
 % finds data for both sexes and all ages, death per 100 000
 IncomeCOD = IncomeCOD(:, [3, 7, 9]);
-
-
-
-
 
 %x = strcmpi(LowerMidCOD(:, 2), UpperMidCOD(:, 2)) & strcmpi(LowerMidCOD(:, 1), UpperMidCOD(:, 1));
 %verified data sorted correctly with disease and year
