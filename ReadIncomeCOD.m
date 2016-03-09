4c4cde6757fe8df9f8bd7eea00562bfef25148a2
fttang <mictang@uw.edu>
2016-03-08 22:28:06 -0800

    updated ReadIncomeCOD, now accepts year as input

--- ReadIncomeCOD.m
+++ ReadIncomeCOD.m
@@ -1,13 +1,16 @@
-function [IncomeCOD] = ReadIncomeCOD(IncomeCOD)
+function [IncomeCOD] = ReadIncomeCOD(IncomeCOD, year)
 
-IncomeCOD = sortrows(IncomeCOD,'Causes','ascend');
+IncomeCOD = sortrows(IncomeCOD, 'Causes', 'ascend');
 IncomeCOD = sortrows(IncomeCOD, 'Year', 'ascend');
 
 % this could be written as a function
+IncomeCOD = IncomeCOD(strcmp(table2array(IncomeCOD(:, 'Year')), year), :);
+IncomeCOD = IncomeCOD(strcmpi(table2array(IncomeCOD(:, 'Sex')), 'Both sexes'), :);
+IncomeCOD = IncomeCOD(:, {'Year', 'Causes', 'Numeric'});
 IncomeCOD = table2array(IncomeCOD);
-IncomeCOD = IncomeCOD(strcmpi(IncomeCOD(:, 5), 'Both sexes'), :);
+;
 % finds data for both sexes and all ages, death per 100 000
-IncomeCOD = IncomeCOD(:, [3, 7, 9]);
+
 
 %x = strcmpi(LowerMidCOD(:, 2), UpperMidCOD(:, 2)) & strcmpi(LowerMidCOD(:, 1), UpperMidCOD(:, 1));
 %verified data sorted correctly with disease and year
--- income_cause_of_death.m
+++ income_cause_of_death.m
@@ -4,11 +4,14 @@ LowIncomeCOD = readtable('CauseOfDeathLow.csv');
 LowerMidCOD = readtable('CauseOfDeathLowerMid.csv');
 UpperMidCOD = readtable('CauseOfDeathUpperMid.csv');
 HighIncomeCOD = readtable('CauseOfDeathHigh.csv');
+
+%%
 % pull death cause of both sexes, 2000 and 2010
-LowIncomeCOD = ReadIncomeCOD(LowIncomeCOD);
-LowerMidCOD = ReadIncomeCOD(LowerMidCOD);
-UpperMidCOD = ReadIncomeCOD(UpperMidCOD);
-HighIncomeCOD = ReadIncomeCOD(HighIncomeCOD);
+year = '2012'; %or 2000
+LowIncomeCOD = ReadIncomeCOD(LowIncomeCOD, year);
+LowerMidCOD = ReadIncomeCOD(LowerMidCOD, year);
+UpperMidCOD = ReadIncomeCOD(UpperMidCOD, year);
+HighIncomeCOD = ReadIncomeCOD(HighIncomeCOD, year);
 % mean of each cause in lower and upper middle income countries
 MidIncomeCOD = (str2double(LowerMidCOD(:, 3)) + str2double(UpperMidCOD(:, 3)) )/2;
 
@@ -17,39 +20,25 @@ MidIncomeCOD = (str2double(LowerMidCOD(:, 3)) + str2double(UpperMidCOD(:, 3)) )/
 % intial visual comparison of data
 figure;
 hold on;
-% plotting only the 2000 data or the 2012 data, not both at once
-plot(str2double(LowIncomeCOD(find(str2double(LowIncomeCOD(1:158, 3))) > 0, 3)  ), '*');
-plot(str2double(LowerMidCOD(find(str2double(LowerMidCOD(1:158, 3))) > 0, 3)  ), '.');
-plot(str2double(UpperMidCOD(find(str2double(UpperMidCOD(1:158, 3))) > 0, 3)  ), '.');
-plot(str2double(HighIncomeCOD(find(str2double(HighIncomeCOD(1:158, 3))) > 0, 3)  ), 'o');
+plot(str2double(LowIncomeCOD(find(str2double(LowerMidCOD(:, 3))) > 0, 3)  ), '.');
+plot(str2double(LowerMidCOD(find(str2double(LowerMidCOD(:, 3))) > 0, 3)  ), '^');
+plot(str2double(UpperMidCOD(find(str2double(LowerMidCOD(:, 3))) > 0, 3)  ), '^');
+plot(str2double(HighIncomeCOD(find(str2double(LowerMidCOD(:, 3))) > 0, 3)  ), 'o');
 title('Causes of Death in Low, Lower Middle, Uppper Middle and High Income Countries in 2000');
 legend('lower income', 'lower mid income', 'upper mid income', 'high income');
 ylabel('deaths per 10 000 population');
 xlabel('cause, too many to display at this moment');
 
 print('intial','-dpng','-r300'); 
-%% work in progress
-% threshold value for significant deaths per 10 000 population needs to be
-% determined
-[row, column] = size(HighIncomeCOD);
-low = ones(row, 1);
-lmid= ones(row, 1)*2;
-umid = ones(row, 1)*3;
-high = ones(row, 1)*4;
-
-%obj = fitcdiscr([str2double(LowCOD(:, 3)); 
-%    str2double(LowerMidCOD(:, 3)); 
-%    str2double(UpperMidCOD(:, 3)); str2double(HighIncomeCOD(:, 3))],  [low; lmid; umid; high])
-
 %%
 % 2012
-[sorted,sortedIndex] = sort(str2double(HighIncomeCOD(159:316, 3)),'descend');
+[sorted,sortedIndex] = sort(str2double(HighIncomeCOD(:, 3)),'descend');
 HighSortValueIndices = sortedIndex(1:10);
 
-[sorted,sortedIndex] = sort(MidIncomeCOD(159:316),'descend');
+[sorted,sortedIndex] = sort(MidIncomeCOD,'descend');
 MidSortValueIndices = sortedIndex(1:10);
 
-[sorted,sortedIndex] = sort(str2double(LowIncomeCOD(159:316, 3)),'descend');
+[sorted,sortedIndex] = sort(str2double(LowIncomeCOD(:, 3)),'descend');
 LowSortValueIndices = sortedIndex(1:10);
 
 TopDeathIndex = unique([HighSortValueIndices; MidSortValueIndices; LowSortValueIndices]);
@@ -60,22 +49,23 @@ TopDeathIndex = unique([HighSortValueIndices; MidSortValueIndices; LowSortValueI
 %cod(:, 4) = LowIncomeCOD(TopDeathIndex);
 
 %%
-stddev2000=[1:3, 5:158];
-stddev2012=[159:161, 163:316]; 
+stddev=[1:3, 5:158];
 % removed 'all causes'
+% the stddev still will be inflated a little bit but the difference is
+% still significant for the causes we are looking at
 
 figure;
 hold on;
 % [1, 2, 4, 6, 7] are some of the top communicable\infectious diseases
 
 
-errorlow = std(str2double(LowIncomeCOD(stddev2012, 3)) );
+errorlow = std(str2double(LowIncomeCOD(stddev, 3)) );
 errorbar(str2double(LowIncomeCOD(TopDeathIndex, 3)), errorlow*ones(size(TopDeathIndex)), 'rs-');
 
-errormid = std(MidIncomeCOD(stddev2012) );
+errormid = std(MidIncomeCOD(stddev) );
 errorbar(MidIncomeCOD(TopDeathIndex), errormid*ones(size(TopDeathIndex)), 'b^-');
 
-errorhigh = std(str2double(HighIncomeCOD(stddev2012, 3)) );
+errorhigh = std(str2double(HighIncomeCOD(stddev, 3)) );
 errorbar(str2double(HighIncomeCOD(TopDeathIndex, 3)), errorhigh*ones(size(TopDeathIndex)), 'go-');
 
 set(gca,'xdir','reverse')
